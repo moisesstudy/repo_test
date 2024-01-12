ce5311d66b53f0d345cf8df28e1f0a8621c23467
moisesstudy <moisesstudytech@gmail.com>
2024-01-12 00:36:46 -0500

    Function Afficher Lignes - Cuarto Cambio

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+def afficher_lignes(n = 10):
+    print("Afficher plusieur lignes avec - comment une piramide")
+    for i in range(n):
+        print("-"*i)
+
 print("Hola Moises - estudio GIT ")
 print("Fichier modifié")
-print("Estoy en la etape de intregratiosn con visual Studio Code")
+print("Estoy en la etape de integrasion con visual Studio Code pour git")
+afficher_lignes(10)
