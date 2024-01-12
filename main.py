34814c97e89046eaed204ad68537ea66213ed0c4
moisesstudy <moisesstudytech@gmail.com>
2024-01-12 01:28:21 -0500

    merge main.p

--- main.py
+++ main.py
@@ -7,3 +7,6 @@ print("Hola Moises - estudio GIT ")
 print("Fichier modifié")
 print("Estoy en la etape de integrasion con visual Studio Code pour git")
 afficher_lignes(10)
+
+print("en cooperation - travailleur 1 ajout. ce ligne")
+print("Travailleur 2 fait ses modifications")
